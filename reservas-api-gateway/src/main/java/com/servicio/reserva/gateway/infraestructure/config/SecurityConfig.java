aa9a3f4ad7a29c8de733df11cfa99d361b807a84
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-14 15:14:01 -0500

    feat(config): configuración del filtro de seguridad para manejar jwt y enrutamiento publico y privado

---
+++ reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/infraestructure/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.servicio.reserva.gateway.infraestructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchange ->
+                    exchange
+                            .pathMatchers("/api/auth/register").permitAll()
+                            .pathMatchers("/api/auth/login").permitAll()
+                            .pathMatchers("/api/auth/refresh").permitAll()
+                            .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(server -> server.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return http.build();
+    }
+}
