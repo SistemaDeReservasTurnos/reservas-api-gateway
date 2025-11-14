1765226285c394638a8846078ce24b050114e2cc
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-14 14:50:18 -0500

    chore: estructura inicial del proyecto y configuración de las dependencias

---
+++ reservas-api-gateway/src/test/java/com/servicio/reserva/gateway/ReservasApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.reserva.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
