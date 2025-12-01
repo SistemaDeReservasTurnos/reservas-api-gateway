008154db52c86e68155d08dee8523e249739040f
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-30 23:28:45 -0500

    test: creación y configuración de tests de integración

--- reservas-api-gateway/src/test/java/com/servicio/reserva/gateway/ReservasApiGatewayApplicationTests.java
+++ reservas-api-gateway/src/test/java/com/servicio/reserva/gateway/ReservasApiGatewayApplicationTests.java
@@ -2,8 +2,18 @@ package com.servicio.reserva.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("local")
+@TestPropertySource(properties = {
+        "bff.client-id=gateway-test",
+        "bff.client-secret=secret-test-key",
+        "eureka.client.enabled=false",
+        "eureka.client.register-with-eureka=false",
+        "eureka.client.fetch-registry=false"
+})
 class ReservasApiGatewayApplicationTests {
 
     @Test
