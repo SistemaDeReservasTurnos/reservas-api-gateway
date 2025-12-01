844137d5b2fbb7d857072c95688d4a4f4124582e
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-12-01 00:31:48 -0500

    Revert "style(dto): se añadio la propiedad @AllArgsConstructor para evitar la creación de objetos incompletos"
    This reverts commit 54a0ce07abb52f82f95429bcdd88f2e8e811c6da.

--- reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LogoutRequest.java
+++ reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LogoutRequest.java
@@ -1,13 +1,11 @@
 package com.servicio.reserva.gateway.application.dto.requests;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class LogoutRequest {
     @NotBlank(message = "Token is required.")
     private String token;
--- reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/RefreshTokenRequest.java
+++ reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/RefreshTokenRequest.java
@@ -1,13 +1,11 @@
 package com.servicio.reserva.gateway.application.dto.requests;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class RefreshTokenRequest {
     @NotBlank(message = "Refresh token is required")
     private String refresh_token;
