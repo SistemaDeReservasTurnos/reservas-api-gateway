95ba2accae5ffe99a2a3d711f2e989c0a0985683
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-30 23:25:06 -0500

    style: permitir usar el patron Builder en DTO's

--- reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LogoutRequest.java
+++ reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LogoutRequest.java
@@ -1,9 +1,11 @@
 package com.servicio.reserva.gateway.application.dto.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class LogoutRequest {
     @NotBlank(message = "Token is required.")
     private String token;
