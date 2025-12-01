54a0ce07abb52f82f95429bcdd88f2e8e811c6da
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-12-01 00:02:01 -0500

    style(dto): se añadio la propiedad @AllArgsConstructor para evitar la creación de objetos incompletos

--- reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LoginRequest.java
+++ reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LoginRequest.java
@@ -3,11 +3,13 @@ package com.servicio.reserva.gateway.application.dto.requests;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
--- reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LogoutRequest.java
+++ reservas-api-gateway/src/main/java/com/servicio/reserva/gateway/application/dto/requests/LogoutRequest.java
@@ -1,11 +1,13 @@
 package com.servicio.reserva.gateway.application.dto.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class LogoutRequest {
     @NotBlank(message = "Token is required.")
     private String token;
